e038075d38fa537d291ea71db6b38cc7ec165cb9
krishnendu basak <krisauz17@yahoo.com>
2022-09-11 21:35:27 +1000

    phase-0-pac-3-intro-to-functions-lab done

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ function shout(string) {
   function whisper(string) {
     return string.toLowerCase();
   }
+  console.log(shout('hello'));
+  console.log(whisper("HELLO"));
+  
+  function logShout(string){
+    console.log(string.toUpperCase());
+    return string.toUpperCase();
+  }
+  function logWhisper(string){
+    console.log(string.toLowerCase());
+    return string.toLowerCase();
+  }
+  console.log(logShout("hello"));
+  console.log(logWhisper("HELLO"));
+
+  function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()===string){
+      return `I can't hear you!`
+    }
+    return 'Yes INDEED!'
+    }
+    if (string === "Let's have dinner together!"){
+      console.log ("I would like to!")
+      return "I would like to!"
+    }    
+  
